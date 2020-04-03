bf4c9dc6ce9f9b1f5fa3a1ec0b3ec55196d97930
Adam Kiss <kisssandoradam@gmail.com>
2020-04-03 21:10:41 +0200

    first commit

---
+++ src/main/scala/com/example/demo/DemoScalaApplication.scala
@@ -0,0 +1,7 @@
+package com.example.demo
+
+object DemoScalaApplication extends App {
+
+  println("Hello Scala!")
+
+}
---
+++ src/tools/java/com/example/demo/tools/ToolExample.java
@@ -0,0 +1,9 @@
+package com.example.demo.tools;
+
+public class ToolExample {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Tools!");
+    }
+
+}
